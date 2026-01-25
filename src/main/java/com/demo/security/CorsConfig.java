469a23dddb327ab88d3c3a8ec8c645ab817a7861
jason <motmet75@gmail.com>
2026-01-25 09:58:36 +0700

    add Cors

--- src/main/java/com/demo/security/CorsConfig.java
+++ src/main/java/com/demo/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/bom/api**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "http://15.165.215.69:5173")
                         .allowedMethods("*");
             }
         };
