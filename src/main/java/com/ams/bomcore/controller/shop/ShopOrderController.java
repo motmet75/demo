cee40ae34aac33adfc13f47aea6f1d892ffc0c42
motmet75 <motmet75@gmail.com>
2026-06-08 13:41:21 +0700

    xx

--- src/main/java/com/ams/bomcore/controller/shop/ShopOrderController.java
+++ src/main/java/com/ams/bomcore/controller/shop/ShopOrderController.java
@@ -11,13 +11,21 @@ import com.ams.bomcore.service.shop.ShopPricingService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.util.*;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"})
 public class ShopOrderController {
 
     private final ShopOrderService shopOrderService;
