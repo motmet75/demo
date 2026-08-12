fc7775ae68f84823d0b91c37937bdeeb020a6a64
motmet75 <motmet75@gmail.com>
2026-08-12 23:34:24 +0700

    confirm ipad4 logout

--- bom-frontend/vite.config.js
+++ bom-frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-2  base: '/bom-inventory/',
+  base: '/bom-inventory/',
   build: {
     rollupOptions: {
       output: {
