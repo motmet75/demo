1851994dc96e0c86de3c3443fb389d16b10f8edf
motmet75 <motmet75@gmail.com>
2026-08-25 14:33:01 +0700

    QR pay slip number special character

--- bom-frontend/src/features/shoporder/ShopOrderGrid.jsx
+++ bom-frontend/src/features/shoporder/ShopOrderGrid.jsx
@@ -3007,7 +3007,9 @@ function CombinedReceiptDialog({ token, orderRangeParams = {}, modelMetaMap = {}
     ? `https://img.vietqr.io/image/${bankConfig.bankBin}-${bankConfig.bankAccountNumber}-qr_only.png`
       + `?amount=${Math.round(unpaidTotal)}`
       + `&addInfo=${encodeURIComponent(
-          slipNumber ? `PHIEU ${slipNumber}` : (token?.substring(0, 12) || 'combined')
+          slipNumber
+              ? `PHIEU ${slipNumber} ${token?.substring(0, 12) || ''}`
+              : (token?.substring(0, 12) || 'combined')
       )}`
       + `&accountName=${encodeURIComponent(bankConfig.bankAccountName || '')}`
     : null
